77ebe9035e4f220dcb031176e0b81a991562b7ad
Arun Soman <aarunsoman@gmail.com>
2016-10-02 13:21:42 +0530

    Update tpblock.js

--- generators/javascript/tpblock.js
+++ generators/javascript/tpblock.js
@@ -1,108 +1,69 @@
-Blockly.JavaScript['line_extractor'] = function(block) {
-  var text_get = block.getFieldValue('get');
-  var text_delim = block.getFieldValue('delim');
-  var dropdown_data_type = block.getFieldValue('data_type');
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+Blockly.JavaScript['extractor'] = function(block) {
+  var value_line = Blockly.JavaScript.valueToCode(block, 'line', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_file = Blockly.JavaScript.valueToCode(block, 'file', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['line_extractor'] = function(block) {
+ var number_get = block.getFieldValue('get');
+  var text_delim = block.getFieldValue('delim');
+  var dropdown_operation = block.getFieldValue('operation');
+  var variable_marker = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('marker'), Blockly.Variables.NAME_TYPE);
+  var value_next_marker = Blockly.JavaScript.valueToCode(block, 'next_marker', Blockly.JavaScript.ORDER_ATOMIC);
+    var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['file_name_extractor'] = function(block) {
-  var text_get = block.getFieldValue('get');
+ var number_get = block.getFieldValue('get');
   var text_delim = block.getFieldValue('delim');
-  var dropdown_data_type = block.getFieldValue('data_type');
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
+  var dropdown_operation = block.getFieldValue('operation');
+  var variable_marker = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('marker'), Blockly.Variables.NAME_TYPE);
+  var value_next_marker = Blockly.JavaScript.valueToCode(block, 'next_marker', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+
+Blockly.JavaScript['field_extractor'] = function(block) {
+  var number_get = block.getFieldValue('get');
+  var text_delim = block.getFieldValue('delim');
+  var dropdown_operation = block.getFieldValue('operation');
+  var variable_marker = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('marker'), Blockly.Variables.NAME_TYPE);
+  var value_next_marker = Blockly.JavaScript.valueToCode(block, 'next_marker', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = '...';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.JavaScript['field_extractor'] = function(block) {
-  var text_get = block.getFieldValue('get');
-  var text_delim = block.getFieldValue('delim');
-  var dropdown_data_type = block.getFieldValue('data_type');
+
+Blockly.JavaScript['transform'] = function(block) {
   var statements_name = Blockly.JavaScript.statementToCode(block, 'NAME');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
 };
 
-Blockly.JavaScript['string_binary_transform'] = function(block) {
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m2 = Blockly.JavaScript.valueToCode(block, 'm2', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript['binary_string_operator'] = function(block) {
+  var variable_m1 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('m1'), Blockly.Variables.NAME_TYPE);
+  var dropdown_operator = block.getFieldValue('operator');
+  var variable_m2 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('m2'), Blockly.Variables.NAME_TYPE);
+  var variable_result = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('result'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = '...;\n';
+  return code;
 };
 
-Blockly.JavaScript['string_unary_transform'] = function(block) {
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
+Blockly.JavaScript['unary_string_operator'] = function(block) {
+  var dropdown_operator = block.getFieldValue('operator');
+  var variable_m2 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('m2'), Blockly.Variables.NAME_TYPE);
+  var variable_result = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('result'), Blockly.Variables.NAME_TYPE);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['math_binary_transform'] = function(block) {
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m2 = Blockly.JavaScript.valueToCode(block, 'm2', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['math_unary_transform'] = function(block) {
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-
-Blockly.JavaScript['date_binary_transform'] = function(block) {
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m2 = Blockly.JavaScript.valueToCode(block, 'm2', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['date_unary_transform'] = function(block) {
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['logic_binary_transform'] = function(block) {
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m2 = Blockly.JavaScript.valueToCode(block, 'm2', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['logic_unary_transform'] = function(block) {
-  var dropdown_operation = block.getFieldValue('operation');
-  var value_m1 = Blockly.JavaScript.valueToCode(block, 'm1', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = '...;\n';
+  return code;
 };
